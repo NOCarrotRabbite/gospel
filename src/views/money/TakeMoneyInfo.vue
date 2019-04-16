4c6276b9bc6096d157578fbecb32cef1e80ecc9d
1473 <995069790@qq.com>
2019-04-16 21:02:49 +0800

    20190415陈静

--- src/views/money/TakeMoneyInfo.vue
+++ src/views/money/TakeMoneyInfo.vue
@@ -1,31 +1,48 @@
 <template>
   <div class="take-money-box">
     <header>
-      <a href="./my_shop"><i class="iconfont icon-fanhui"></i></a>
-      <h4>提款记录</h4>
-      <i class="iconfont icon-weibiaoti--"></i>
+      <a href="./"><i class="iconfont icon-fanhui"></i></a>
+      <h4>我的提款</h4>
+      <i class="iconfont icon-canshupeizhi"></i>
     </header>
     <main>
       <div class="take-money">
-        <button>我要提款</button>
+        <input class="money" type="text" placeholder="请输入提款金额，至少大于3000"/>
+        <div class="pay-type">
+          <label><input type="radio" name="payType" value="1"/>支付宝</label>
+          <label><input type="radio" name="payType" value="2"/>银行卡</label>
+        </div>
+        <!--<button>提款</button>-->
       </div>
       <div class="take-money-info">
         <h5>提款记录</h5>
         <div class="info-list">
-          <span>2019-4-15 15:36:02</span>
-          <span>- ￥3000.00</span>
+          <span class="take-time">2019-4-15 15:36:02</span>
+          <span class="take-num">- ￥3000.00</span>
         </div>
         <div class="info-list">
-          <span>2019-4-15 15:36:02</span>
-          <span>- ￥3000.00</span>
+          <span class="take-time">2019-4-15 15:36:02</span>
+          <span class="take-num">- ￥3000.00</span>
         </div>
         <div class="info-list">
-          <span>2019-4-15 15:36:02</span>
-          <span>- ￥3000.00</span>
+          <span class="take-time">2019-4-15 15:36:02</span>
+          <span class="take-num">- ￥3000.00</span>
         </div>
         <div class="info-list">
-          <span>2019-4-15 15:36:02</span>
-          <span>- ￥3000.00</span>
+          <span class="take-time">2019-4-15 15:36:02</span>
+          <span class="take-num">- ￥3000.00</span>
+        </div>
+        <div class="info-list">
+          <span class="take-time">2019-4-15 15:36:02</span>
+          <span class="take-num">- ￥3000.00</span>
+        </div>
+        <div class="info-list">
+          <span class="take-time">2019-4-15 15:36:02</span>
+          <span class="take-num">- ￥3000.00</span>
+        </div>
+        <div class="info-list">
+          <span class="take-time">2019-4-15 15:36:02</span>
+          <span class="take-num">- ￥3000.00</span>
         </div>
       </div>
     </main>
@@ -45,43 +62,68 @@
       display: flex;
       justify-content: space-between;
       align-items: center;
-      height: 2.5rem;
-      color: #20262F;
-      padding: 0.5rem 1rem;
+      //height: 2.5rem;
+      color: #4D2FB5;
+      padding: 1.5rem 1rem 0 1rem;
+      h4 {
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+      }
       i {
         font-size: 1.3rem;
       }
       a {
-        color: #20262F;
+        color: #4D2FB5;
       }
     }
     main {
+      overflow: scroll;
       box-sizing: border-box;
       flex-grow: 1;
-      padding: 0 1rem;
+      padding: 1.5rem 1rem 1rem 1rem;
       .take-money {
-        background-color:  #7261D1;
+        background: linear-gradient(to bottom right, #884CC3 0%, #6F47B9 50%, #4D2FB5 100%);
         text-align: center;
         padding: 4rem 0;
-        border-radius: 5px;
+        border-radius: 10px;
+        .money {
+          height: 2rem;
+          width: 80%;
+          border-radius: 5px;
+        }
         button {
           height: 2.8rem;
           width: 80%;
-          //background-color: #4738BC;
-          color: #20262F;
+          background-color: #fff;
+          // color: #7261D1;
+          color: #16191C;
           border-radius: 5rem;
-          font-size: 1.1rem;
+          font-size: 1rem;
+          font-weight: 600;
         }
       }
       .take-money-info {
         padding: 1rem 0;
+        h5 {
+          color: #4C5660;
+          padding-bottom: 0.5rem;
+        }
         .info-list {
           background-color: #fff;
           margin-top: 0.5rem;
           padding: 0.5rem 0.5rem;
           display: flex;
           justify-content: space-between;
-          border-radius: 5px;
+          align-items: center;
+          border-radius: 10px;
+          .take-time {
+            font-size: 0.8rem;
+            color: #A4AAAF;
+          }
+          .take-num {
+            color: #6A59CE;
+          }
         }
       }
     }
